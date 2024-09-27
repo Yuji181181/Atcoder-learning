a11367b0a5f024f22893a797624fd95963d5669e
Yuji181181 <s1f102402552@iniad.org>
2024-09-27 18:28:38 +0900

    52

---
+++ Golden-rulebook-py/chapter 8/A52.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+Q = int(input())
+queries = []
+for i in range(Q):
+    queries.append(input().split())
+
+S = deque()
+for q in queries:
+    if q[0] == "1":
+        S.append(q[1])
+    if q[0] == "2":
+        print(S[-1])
+    if q[0] == "3":
+        S.popleft()
